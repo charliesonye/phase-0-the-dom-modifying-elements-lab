cd317b0408b9af65b11df1a263ddfe604cbb698d
Charles <c_onyemem@live.com>
2021-12-15 17:05:46 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const body = document.querySelector('body')
+const main = document.getElementById('main')
+
+body.remove(main)
+
+
+const newHeader = document.createElement('H1')
+newHeader.textContent = 'Charles is the champion'
+newHeader.id = 'victory'
